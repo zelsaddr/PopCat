bc9a58bd5fb87586082450f0a36524699933ce6f
Izzeldin Addarda <zeldin26@gmail.com>
2021-08-15 16:07:49 +0700

    update clicked times, can headless mode

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const puppeteer = require('puppeteer');
-
-const klik = async page => {
-    await page.click("#app > div");
-    await page.click("#app > div", {button: 'right'});
-}
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+const log = require("single-line-log").stdout;
+const klik = async (page) => {
+  await page.click("#app > div");
+  await page.click("#app > div", { button: "right" });
+};
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        devtools: false
-    });
-    const popCatPage = await browser.newPage();
-    await popCatPage.goto("https://popcat.click/", {waitUntil: "networkidle2"});
-    while(true){
-        await klik(popCatPage);
-    }
+  const browser = await puppeteer.launch({
+    headless: true,
+    devtools: false,
+  });
+  const popCatPage = await browser.newPage();
+  await popCatPage.goto("https://popcat.click/", { waitUntil: "networkidle2" });
+  while (true) {
+    await klik(popCatPage);
+    let counter = await popCatPage.$("#app > div > div[class*=counter]");
+    let count = await counter.evaluate((el) => el.textContent);
+    log("Clicked times : ", count);
+  }
 })();
-
